477c80f39377355312340b21b6adee52f72f37fd
Maxim Lazarev <maxim.lazarev@compvisionsys.com>
2023-01-08 02:25:10 +0300

    wagon release

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 
 int main() {
     cv::Mat background = cv::imread("../moleworm.jpg");
-    Wagon wagon;
-    wagon.setPosition(100, 100, 45.0);
-    wagon.draw(background);
-    cv::imshow("lab1", background);
-    cv::waitKey(-1);
+    Wagon wagon(background, 30, 40);
+    wagon.drive_1();
+    wagon.drive_2();
 }
--- wagon.h
+++ wagon.h
@@ -1,14 +1,29 @@
 #include <opencv2/opencv.hpp> 
+#include <vector>
 
 class Wagon {
 public:
-    Wagon(double side = 40) : side_(side) {};
-    void draw(cv::Mat background);
-    void setPosition(int x, int y, double arg);
+    Wagon(cv::Mat background, double first_side = 40, double second_side = 30);
+    void drive_1();
+    void drive_2();
+    void draw_wagon(cv::Mat background);
+    void draw_full_trajectory(cv::Mat background);
+    void set_position(int x, int y, double arg);
     
 private:
-    const double side_;
+    struct TrajectoryNode {
+        cv::Point2d point;
+        double angle;
+    };
+
+    const double a_side_;
+    const double b_side_;
+
+    const cv::Mat background_;
+
     int x_ = 0;
     int y_ = 0;
     double angle_ = 0;
+
+    std::vector<TrajectoryNode> trajectory_;
 };
